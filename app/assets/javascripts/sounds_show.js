2750a41c19226a397204e05d86451b433b0db593
Chester Law <chesterlaw@me.com>
2016-04-15 00:20:36 +0900

    Add comments to sounds_show.js

--- app/assets/javascripts/sounds_show.js
+++ app/assets/javascripts/sounds_show.js
@@ -4,11 +4,13 @@ $(document).ready(function() {
   if (audio) { main(); }
 
   function main() {
+    // Update audio currentTime display for user.
     audio.ontimeupdate = function() {
       var currentTimeElement = document.querySelector('.sounds-show .current-time')
       currentTimeElement.innerHTML = Math.floor(audio.currentTime*1000);
     };
 
+    // Play sections when Play button is clicked.
     $('.section-box button').on('click', function() {
         var startTime = $(this).parents('.section-box').data('start-time')/1000;
         var endTime = $(this).parents('.section-box').data('end-time')/1000;
