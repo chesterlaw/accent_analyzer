a870dce5daea5d8c27091ab5bd2d695c177dbefe
Chester Law <chesterlaw@me.com>
2016-04-14 23:33:56 +0900

    More robust audio currentTime updating

--- app/assets/javascripts/sounds_show.js
+++ app/assets/javascripts/sounds_show.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   var audio = document.querySelector('.sounds-show audio');
 
   if (audio) {
-    audio.onseeking = function() {
+    audio.ontimeupdate = function() {
       var currentTimeElement = document.querySelector('.sounds-show .current-time')
       currentTimeElement.innerHTML = Math.floor(audio.currentTime*1000);
     };
