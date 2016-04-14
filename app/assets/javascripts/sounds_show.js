734a5a8d24ba7207cc3661e2e867deac04c46adc
Chester Law <chesterlaw@me.com>
2016-04-15 00:16:28 +0900

    Add functionality to play button for sections

--- app/assets/javascripts/sounds_show.js
+++ app/assets/javascripts/sounds_show.js
@@ -1,10 +1,31 @@
 $(document).ready(function() {
   var audio = document.querySelector('.sounds-show audio');
 
-  if (audio) {
+  if (audio) { main(); }
+
+  function main() {
     audio.ontimeupdate = function() {
       var currentTimeElement = document.querySelector('.sounds-show .current-time')
       currentTimeElement.innerHTML = Math.floor(audio.currentTime*1000);
     };
+
+    $('.section-box button').on('click', function() {
+        var startTime = $(this).parents('.section-box').data('start-time')/1000;
+        var endTime = $(this).parents('.section-box').data('end-time')/1000;
+        // var src = document.querySelector('.sound audio').src;
+        // console.log(src);
+
+        audio.currentTime = startTime;
+        audio.play()
+        checkIfSectionEnded();
+
+        function checkIfSectionEnded() {
+          if (audio.currentTime <= endTime) {
+            requestAnimationFrame(checkIfSectionEnded);
+          } else {
+            audio.pause();
+          }
+        }
+    });
   }
 });
