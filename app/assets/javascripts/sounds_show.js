a51fb965be9baf8dab2dcd041ec2a38719575652
Chester Law <chesterlaw@me.com>
2016-04-15 00:40:42 +0900

    Extract playFromHereToHere function

--- app/assets/javascripts/sounds_show.js
+++ app/assets/javascripts/sounds_show.js
@@ -12,22 +12,9 @@ $(document).ready(function() {
 
     // Play sections when Play button is clicked.
     $('.section-box button').on('click', function() {
-        var startTime = $(this).parents('.section-box').data('start-time')/1000;
-        var endTime = $(this).parents('.section-box').data('end-time')/1000;
-        // var src = document.querySelector('.sound audio').src;
-        // console.log(src);
-
-        audio.currentTime = startTime;
-        audio.play()
-        checkIfSectionEnded();
-
-        function checkIfSectionEnded() {
-          if (audio.currentTime <= endTime) {
-            requestAnimationFrame(checkIfSectionEnded);
-          } else {
-            audio.pause();
-          }
-        }
+      var startTime = $(this).parents('.section-box').data('start-time')/1000;
+      var endTime = $(this).parents('.section-box').data('end-time')/1000;
+      playFromHereToHere(startTime, endTime);
     });
 
     // Visit new section's desired start_time.
@@ -44,5 +31,20 @@ $(document).ready(function() {
         audio.pause();
       }
     });
+  function playFromHereToHere(startTime, endTime) {
+    // var src = document.querySelector('.sound audio').src;
+    // console.log(src);
+
+    audio.currentTime = startTime;
+    audio.play()
+    checkIfSectionEnded();
+
+    function checkIfSectionEnded() {
+      if (audio.currentTime <= endTime) {
+        requestAnimationFrame(checkIfSectionEnded);
+      } else {
+        audio.pause();
+      }
+    }
   }
 });
