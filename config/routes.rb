a943c676dd6ccae55f492888341e087bd1abf1f6
Chester Law <chesterlaw@me.com>
2016-04-28 01:56:02 +0900

    Set root to sounds#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'sounds#index'
+
   resources :sections
   resources :sounds
   # The priority is based upon order of creation: first created -> highest priority.
